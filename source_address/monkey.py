12f3da9594bbcc13b0079e320e3eba423d8caa34
Viktor Stískala <viktor@stiskala.cz>
2014-04-17 13:07:18 +0200

    Fixed import

--- source_address/monkey.py
+++ source_address/monkey.py
@@ -2,13 +2,11 @@ import socket
 real_create_conn = socket.create_connection
 
 
-_GLOBAL_DEFAULT_TIMEOUT = object()
-
 class MonkeySocket:
     _source_address = ('', 0)
 
     def patch_socket(self):
-        def _create_connection(address, timeout=_GLOBAL_DEFAULT_TIMEOUT, *args):
+        def _create_connection(address, timeout=socket._GLOBAL_DEFAULT_TIMEOUT, *args):
             return real_create_conn(address, timeout, self._source_address)
         socket.create_connection = _create_connection
 
